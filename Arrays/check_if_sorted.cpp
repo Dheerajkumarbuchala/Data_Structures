df4649544b459024b042394eda6a98201ed1dd67
Dheerajkumarbuchala <buchaladheerajkumar@gmail.com>
2021-05-13 12:53:56 +0530

    check if array is sorted or not

---
+++ Arrays/check_if_sorted.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+bool sorted_or_not(int a[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]>=a[i+1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(void)
+{
+    int n;
+    cout<<"Size of array : ";
+    cin>>n;
+    int a[n];
+    cout<<"Enter the elements of array : "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<sorted_or_not(a,n)<<endl;
+}
