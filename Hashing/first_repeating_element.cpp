d0cfcf0b3505e57dbd670dea0cc5112d69e55df6
Dheerajkumarbuchala <buchaladheerajkumar@gmail.com>
2021-06-22 23:57:50 +0530

    First repeating element

---
+++ Hashing/first_repeating_element.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+void first_repeating_element(int a[],int n)
+{
+    map<int,int>m;
+    int k = -1;
+    for(int i=0;i<n;i++)
+    {
+        m[a[i]]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(m[a[i]]>1)
+        {
+            k = i+1;
+        }
+    }
+    cout<<k;
+}
+
+int main(void)
+{
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int a[n];
+    cout<<"Enter the elements of the array : "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    first_repeating_element(a,n);
+}
