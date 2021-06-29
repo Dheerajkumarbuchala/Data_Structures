a77c1551575e732871e19d653395ed4f7a9fa08f
Dheerajkumarbuchala <buchaladheerajkumar@gmail.com>
2021-06-29 19:35:54 +0530

    Insert in the beginning.

---
+++ Linked Lists/Insert_in_beg.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+
+struct node{
+    int data;
+    node * next;
+    node(int k)
+    {
+        data = k;
+        next = NULL;
+    }
+};
+
+void insert_in_beginning(node * head)
+{
+    int a = 0;
+    cout<<"Enter the data to insert in beginning : ";
+    cin>>a;
+    node * newnode = new node(a);
+    newnode->next = head;
+    head = newnode;
+    node *temp;
+    temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void bulid_linked_list(int n)
+{
+    node *head = NULL,*temp = NULL;
+    int a = 0;
+    cout<<"Enter the head data : ";
+    cin>>a;
+    head = new node(a);
+    temp = head;
+    while(n-1>0)
+    {
+        int b = 0;
+        cout<<"Enter the data to be inserted : ";
+        cin>>b;
+        node *newnode = new node(b);
+        temp->next = newnode;
+        temp = newnode;
+        n = n-1;
+    }
+    temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+    insert_in_beginning(head);
+}
+
+int main(void)
+{
+    int n = 0;
+    cout<<"Enter the no of nodes to initialize the linked list : ";
+    cin>>n;
+    bulid_linked_list(n);
+}
